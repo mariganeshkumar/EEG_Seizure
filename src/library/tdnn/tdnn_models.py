91477320cd60194a99ce0d7098944aa1665a2767
Mari Ganesh Kumar M <mariganeshkumar@gmail.com>
2020-05-12 14:30:31 +0530

    removed variance and added context

--- src/library/tdnn/tdnn_models.py
+++ src/library/tdnn/tdnn_models.py
@@ -14,8 +14,8 @@ def get_multichannel_tdnn_model(feat_size, num_classes, num_channels, hidden_lay
             channel_data[i] = tf.squeeze(channel_data[i], [1, ])
         return channel_data
 
-    tdnn_layer1 = TDNN(int(hidden_layer_config[0]), (0,), padding='same', activation="sigmoid", name="TDNN1")
-    tdnn_layer2 = TDNN(int(hidden_layer_config[1]), input_context=(0,), padding='same', activation="sigmoid", name="TDNN2")
+    tdnn_layer1 = TDNN(int(hidden_layer_config[0]), (-2,0,2), padding='same', activation="sigmoid", name="TDNN1")
+    tdnn_layer2 = TDNN(int(hidden_layer_config[1]), input_context=(-2,0,2), padding='same', activation="sigmoid", name="TDNN2")
     average = Lambda(lambda xin: K.mean(xin, axis=1), output_shape=(int(hidden_layer_config[1]),))
     variance = Lambda(lambda xin: K.std(xin, axis=1), output_shape=(int(hidden_layer_config[1]),))
 
@@ -33,8 +33,8 @@ def get_multichannel_tdnn_model(feat_size, num_classes, num_channels, hidden_lay
     vv = Concatenate()(vars)
     k1 = Concatenate()([mv, vv])
     d1 = Dense(int(hidden_layer_config[2]), activation='sigmoid', name='x_vector')(mv)
-    #d2 = Dense(int(hidden_layer_config[3]), activation='sigmoid', name='x_vector_2')(d1)
-    output = Dense(num_classes, activation='softmax', name='dense_2')(d1)
+    d2 = Dense(32, activation='sigmoid', name='x_vector_2')(d1)
+    output = Dense(num_classes, activation='softmax', name='dense_2')(d2)
     if get_embedding:
         model = keras.Model(inputs=inputs, outputs=d1)
     else:
